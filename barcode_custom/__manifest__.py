1a68bf87c82713f225a7ebcfaad4921d982934dc
yankinmax <yankinmk@gmail.com>
2019-06-25 22:05:08 +0300

    added custom location barcode

---
+++ barcode_custom/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Barcode Custom",
+    "version": "1.0",
+    "category": "Sales",
+    "author": 'Maksym Yankin | @yankinmax',
+    "description": """
+    """,
+    "depends": ['product', 'stock'],
+    "data": [
+        'views/product_template.xml',
+        'report/product_report.xml',
+    ],
+    "installable": True,
+}
