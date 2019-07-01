1736ff956e4b4bc03a59a0a41f34be20b71e0699
yankinmax <yankinmk@gmail.com>
2019-07-01 13:16:42 +0300

    added custom products filter

---
+++ purchase_filter_products_supplier/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Purchase filter products by supplier",
+    "version": "1.0",
+    "category": "Purchase",
+    "author": 'Maksym Yankin | @yankinmax',
+    "description": """
+    """,
+    "depends": ['purchase'],
+    "installable": True,
+}
---
+++ purchase_filter_products_supplier/models/purchase_order.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+from odoo.exceptions import Warning
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    # @api.multi
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        try:
+            result = super(PurchaseOrderLine, self).onchange_product_id()
+
+            supplier_infos = self.env['product.supplierinfo'].search([('name', '=', partner_id)])
+            product_ids = self.env['product.product']
+            for supplier_info in supplier_infos:
+                 product_ids += supplier_info.product_tmpl_id.product_variant_ids
+
+            result.update({'domain': {'product_id': ['&', ('id', 'in', product_ids.ids), ('purchase_ok', '=', True)]}})
+
+            return result
+        except Exception as e:
+            raise Warning(_("Please choose a vendor first."))
