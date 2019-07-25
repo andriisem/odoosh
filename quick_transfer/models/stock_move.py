df5d22c6984451b4913a47002c428640a941ea90
yankinmax <yankinmk@gmail.com>
2019-07-25 17:38:56 +0300

    fixed availability issue

--- quick_transfer/models/stock_move.py
+++ quick_transfer/models/stock_move.py
@@ -9,11 +9,17 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     @api.multi
-    @api.depends('availability')
     @api.constrains('product_uom_qty')
     def onchange_product_uom_qty(self):
         for record in self:
-            if record.product_uom_qty > record.availability:
-                raise UserError('''Quantity of product that is planned to be moved can't be greater
-                                    than quantity what is available in the source location.''')
-        return
+            child_locations = record.location_id.child_ids
+            initial_demand = record.product_uom_qty
+            location_product_id = record.product_id.id
+            for i in child_locations:
+                child_quants = i.quant_ids    
+                for z in child_quants:                    
+                    if location_product_id == z.product_id.id:                  
+                        if initial_demand > z.quantity:
+                            raise UserError('''Quantity of product that is planned to be moved can't be greater
+                                                than quantity what is available in the source location.''')
+            return
