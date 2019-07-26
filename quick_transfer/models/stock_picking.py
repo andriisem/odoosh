9a3c8964f10948bf686728a0568e87e010cc89e0
yankinmax <yankinmk@gmail.com>
2019-07-26 19:18:33 +0300

    added necessary prefilled changes location

--- quick_transfer/models/stock_picking.py
+++ quick_transfer/models/stock_picking.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 class Picking(models.Model):
     _inherit = 'stock.picking'
@@ -12,3 +12,21 @@ class Picking(models.Model):
     custom_location_dest_id = fields.Many2one(
         'stock.location', "Destination Location",
         )
+    location_id = fields.Many2one(
+        'stock.location', 'Source Location',
+        auto_join=False, index=False, required=False,
+        help="Sets a location if you produce at a fixed location. This can be a partner location if you subcontract the manufacturing operations.", compute="_compute_location_id")
+    location_dest_id = fields.Many2one(
+        'stock.location', 'Destination Location',
+        auto_join=False, index=False, required=False,
+        help="Location where the system will stock the finished products.", compute="_compute_location_dest_id")
+
+    @api.depends('custom_location_id')
+    def _compute_location_id(self):
+        for record in self:
+            record.location_id  = record.custom_location_id.id
+
+    @api.depends('custom_location_dest_id')
+    def _compute_location_dest_id(self):
+        for record in self:
+            record.location_dest_id  = record.custom_location_dest_id.id
