fea5716606dc05edad8814f210c5992418109f95
yankinmax <yankinmk@gmail.com>
2019-07-29 12:59:06 +0300

    fixed issue

--- quick_transfer/models/stock_quant.py
+++ quick_transfer/models/stock_quant.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
