0166f96e2da6a01fc7498030971d7cf1474e9d16
yankinmax <yankinmk@gmail.com>
2019-08-19 14:22:01 +0300

    changed cycle locations

--- stock_reporting/models/stock_location.py
+++ stock_reporting/models/stock_location.py
@@ -7,15 +7,19 @@ from odoo import api, fields, models, _
 class Location(models.Model):
     _inherit = "stock.location"
     
-    counted = fields.Char('Counted', compute="_compute_counted", default='No')
+    counted = fields.Char('Counted', compute="_compute_counted", default='NO')
+    move_ids = fields.One2many(
+        'stock.move', 'location_id', string='Created Moves')
 
-    @api.depends('write_date')
+    @api.depends('move_ids')
     def _compute_counted(self):
         context = self.env.context
         before_counting = datetime.strptime(context.get('before_counting'), '%Y-%m-%d %H:%M:%S')
         counting_day = datetime.strptime(context.get('counting_day'), '%Y-%m-%d %H:%M:%S')
         for record in self:
-            if record.write_date > before_counting and record.write_date < counting_day:
+            move_ids_create_date_list = [i.create_date for i in record.move_ids]
+            result = list(filter(lambda x: x > before_counting and x < counting_day, move_ids_create_date_list))
+            if len(result) > 0:
                 record.counted = 'YES'
             else:
                 record.counted = 'NO'
