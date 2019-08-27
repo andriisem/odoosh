fe164b701e6b93e480f6c042a6a89b4c9279c02d
yankinmax <yankinmk@gmail.com>
2019-08-27 21:18:19 +0300

    returned initial barcode_custom

--- barcode_custom/models/product_template.py
+++ barcode_custom/models/product_template.py
@@ -8,7 +8,7 @@ from odoo import api, fields, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    sku = fields.Char('SKU')
+    sku_number = fields.Char('SKU')
 
     # def get_user_preferences(self):
     #     return self.env['user.preferences'].search([('user_id', '=', self.env.user.id)])
