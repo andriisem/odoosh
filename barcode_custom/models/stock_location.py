16b6abac40f1d506c45715f7f7825929f12541bc
Andrey Semko <semko.andrey.i@gmail.com>
2019-09-11 01:52:32 +0300

    refactoring barcode_custom

--- barcode_custom/models/stock_location.py
+++ barcode_custom/models/stock_location.py
@@ -1,43 +1,45 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
-import base64
-from odoo import api, fields, models
-# from odoo.printnodeapi.gateway import Gateway
+from ..printnodeapi import Gateway
+
 
 class Location(models.Model):
     _inherit = "stock.location"
 
     def get_user_preferences(self):
         return self.env['user.preferences'].search([('user_id', '=', self.env.user.id)])
+    
+    def get_api_key(self):
+        apikey = self.env['ir.config_parameter'].sudo().get_param('printnode.api_key')
+        if not apikey:
+            raise UserError(_('''API key for PrintNode required.\n
+                                Save this key in System Parameters with key: printnode.api_key, value: <your api key>
+                                Visit https://app.printnode.com/apikeys for more information.
+                            '''))
+        return apikey
 
     @api.multi
     def print_location_barcode(self):
-        # user preferences with printers information
+        apikey = self.get_api_key()
         user_preferences = self.get_user_preferences()
-        user_name = user_preferences.name
-        printer_type = user_preferences.print_node_ids.printer_type
-        printer_location = user_preferences.print_node_ids.printer_location
-        printer_node_code = user_preferences.print_node_ids.printer_node_code
-
-        # create session with PrintNode via API call 
-        PrintNodeAPIKey = '6zToIF7u9bXIVGfYQMsVUrRqeO45M1iuyug4Lz-LRLk'        
-        gateway=Gateway(url='https://api.printnode.com',apikey=PrintNodeAPIKey)
-        # list of printers
-        printers = gateway.printers(computer=None, printer=None)
-        for i in printers:
-            printer_id = i.id
-            if int(printer_id) == int(printer_node_code):
-                exact_printer_node_code = printer_id
-        # printjobs = gateway.printjobs(printer=printer_node_code)
-
-        # generate pdf file 
-        REPORT_ID = 'barcode_custom.action_report_custom_location_barcode'
-        pdf = self.env.ref(REPORT_ID).render_qweb_pdf(self.ids)
-        b64_pdf = base64.b64encode(pdf[0])
-        # create PrintJob on printer
-        # print(gateway.PrintJob(printer=printer_node_code,options={"copies":1},base64=b64_pdf))
-        # get PrintJob ID on this printer
-        # printjob_id = gateway.printjobs(printer=printer_node_code)[0].id
-        # get state of PrintJob
-        # print(gateway.states(printjob_id)[0][0].state)
+        dymo_code = int(user_preferences.dymo.printer_node_code)
+        if not user_preferences and user_preferences.dymo:
+            raise UserError(_(''' Printer not found, check  Preferences / Printers'''))
+        gateway = Gateway(url='https://api.printnode.com', apikey=apikey)
+        try:
+            printer = gateway.printers(printer=dymo_code)
+        except LookupError:
+            raise UserError(_('No printer with ID ' + str(dymo_code)))
+        pdf_barcode = self.env.ref('barcode_custom.action_report_custom_location_barcode').render_qweb_pdf(self.ids)
+        print_job = gateway.PrintJob(printer=dymo_code, options={"copies":1}, binary=pdf_barcode[0])
+        state = gateway.states(print_job.id)[0][0].state
+        msg = """
+            PrintJob: {} 
+            Computer: {}
+            Printer: {}
+            State:{}
+            """.format(print_job.id, printer.computer.name, printer.name, state)
+        raise UserError(msg)
