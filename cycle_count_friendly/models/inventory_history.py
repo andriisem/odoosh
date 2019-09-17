e2a51f8a5ab3a5e874a10381edb981b65ca6f07b
Andrii Semko <semko.andrey.i@gmail.com>
2019-09-17 11:02:57 +0300

    Minor fix (#14)
    * show only draft state
    
    * dont show quant with 0 qty

--- cycle_count_friendly/models/inventory_history.py
+++ cycle_count_friendly/models/inventory_history.py
@@ -28,6 +28,7 @@ class InventoryHistoty(models.Model):
     def action_play(self):
         self.ensure_one()
         form_view_id = self.env.ref('cycle_count_friendly.view_stock_onhand_count_wizard').id
+        quant_ids = self.location_id.quant_ids.filtered(lambda x: x.quantity != 0)
         return {
             'name': _('Items in %s' % self.location_id.display_name),
             'type': 'ir.actions.act_window',
@@ -36,7 +37,7 @@ class InventoryHistoty(models.Model):
             'view_type': 'form',
             'target': 'new',
             'context': {
-                'default_quant_ids': self.location_id.quant_ids.ids,
+                'default_quant_ids': quant_ids.ids,
                 'location_id': self.location_id.id,
                 "inventory_history_id": self.id
                 },
