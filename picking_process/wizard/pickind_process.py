32d3da935c86b95682dce5bcd1e5478edbea7a3a
Andrey Semko <semko.andrey.i@gmail.com>
2019-11-19 18:17:00 +0200

    in progress

--- picking_process/wizard/pickind_process.py
+++ picking_process/wizard/pickind_process.py
@@ -3,9 +3,7 @@
 import logging
 
 from odoo import _, api, fields, models
-
-_logger = logging.getLogger(__name__)
-
+from odoo.exceptions import UserError
 
 
 class PickingProcess(models.TransientModel):
@@ -13,18 +11,42 @@ class PickingProcess(models.TransientModel):
     _description = 'Picking Process'
 
     picking_location_id = fields.Many2one('stock.location', string='Location', domain="[('usage', '!=', 'view'), ('is_picking_location', '=', True)]")
+    location_id = fields.Many2one('stock.location', string='Location', domain="[('usage', '!=', 'view')]")
+    location_name = fields.Char(string='Loacation Name')
 
 
     @api.multi
     def action_go_to(self):
-        form_view_id = self.env.ref('picking_process.view_picking_location_wizard').id
+        form_view_id = self.env.ref('picking_process.view_scan_location_location_wizard').id
         return {
-            'name': _('GO TO'),
+            'name': _('Picking'),
             'type': 'ir.actions.act_window',
             'views': [(form_view_id, 'form')],
             'view_mode': 'form',
             'view_type': 'form',
             'target': 'new',
             'context': self._context,
+            'res_model': 'picking.process',
+        }
+
+    @api.multi
+    def action_find_location(self):
+        self.ensure_one()
+        location_id = self.location_id.search([('name', '=', self.location_name)])
+        if location_id:
+            self.location_id = location_id
+        else:
+            raise UserError(_('Location not found'))
+        form_view_id = self.env.ref('cycle_count_friendly.view_stock_onhand_count_wizard').id
+    
+        quant_ids = self.location_id.quant_ids.filtered(lambda x: x.quantity != 0)
+        return {
+            'name': _('Picking'),
+            'type': 'ir.actions.act_window',
+            'views': [(form_view_id, 'form')],
+            'view_mode': 'form',
+            'view_type': 'form',
+            'target': 'new',
+            'context': {},
             'res_model': 'picking.process',
         }
